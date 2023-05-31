ef9b4412478bf5a9cf2f98169653b497d22bfbe7
LBRothermel <128320877+LBRothermel@users.noreply.github.com>
2023-05-31 16:53:35 -0400

    Initial Commit

---
+++ MyToDoListApp/MyToDoListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyToDoListAppApp.swift
+//  MyToDoListApp
+//
+//  Created by Lori Rothermel on 5/31/23.
+//
+
+import SwiftUI
+
+@main
+struct MyToDoListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
